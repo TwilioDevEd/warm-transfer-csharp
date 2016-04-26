881b123450143e1553cd86d4997451605e97876e
Agustin Camino <agustin.camino@gmail.com>
2016-04-26 10:13:08 -0500

    Update the application layout

--- WarmTransfer.Web/App_Start/BundleConfig.cs
+++ WarmTransfer.Web/App_Start/BundleConfig.cs
@@ -24,6 +24,7 @@ namespace WarmTransfer.Web
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
+                      "~/Content/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
