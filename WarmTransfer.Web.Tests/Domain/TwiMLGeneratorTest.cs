c354b743eb8d61288f49d509e35790859e344efb
Agustin Camino <agustin.camino@gmail.com>
2016-04-26 15:29:52 -0500

    Add TwiML Generator

---
+++ WarmTransfer.Web.Tests/Domain/TwiMLGeneratorTest.cs
@@ -0,0 +1,24 @@
+﻿using System.Xml.XPath;
+using NUnit.Framework;
+using WarmTransfer.Web.Domain;
+
+namespace WarmTransfer.Web.Tests.Domain
+{
+
+    public class TwiMLGeneratorTest
+    {
+        [Test]
+        public void WhenGenerateConnectConference_ThenGeneratesTwiMLWithDialAndConference()
+        {
+            var response = new TwiMLGenerator().GenerateConnectConference("conference-id", "wait-url", true, false);
+            var conference =
+                response.ToXDocument().XPathSelectElement("Response/Dial/Conference");
+
+
+            Assert.That(conference.Value, Is.EqualTo("conference-id"));
+            Assert.That(conference.Attribute("waitUrl").Value, Is.EqualTo("wait-url"));
+            Assert.That(conference.Attribute("startConferenceOnEnter").Value, Is.EqualTo("true"));
+            Assert.That(conference.Attribute("endConferenceOnExit").Value, Is.EqualTo("false"));
+        }
+    }
+}
---
+++ WarmTransfer.Web/Domain/TwiMLGenerator.cs
@@ -0,0 +1,22 @@
+﻿using Twilio.TwiML;
+
+namespace WarmTransfer.Web.Domain
+{
+    public interface ITwiMLGenerator
+    {
+        TwilioResponse GenerateConnectConference(
+            string conferenceId, string waitUrl, bool startConferenceOnEnter, bool endConferenceOnExit);
+    }
+
+    public class TwiMLGenerator : ITwiMLGenerator
+    {
+        public TwilioResponse GenerateConnectConference(
+            string conferenceId, string waitUrl, bool startConferenceOnEnter, bool endConferenceOnExit)
+        {
+            var response = new TwilioResponse();
+            var conference = new Conference(conferenceId, new {waitUrl, startConferenceOnEnter, endConferenceOnExit});
+
+            return response.Dial(conference);
+        }
+    }
+}
