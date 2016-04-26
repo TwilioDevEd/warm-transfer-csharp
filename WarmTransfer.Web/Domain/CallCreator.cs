8c5185d382b0b0a462601c8c313eb5efe7fd6ea6
Agustin Camino <agustin.camino@gmail.com>
2016-04-26 14:11:39 -0500

    Add call creator

---
+++ WarmTransfer.Web/Domain/CallCreator.cs
@@ -0,0 +1,25 @@
+﻿using Twilio;
+
+namespace WarmTransfer.Web.Domain
+{
+    public interface ICallCreator
+    {
+        void CallAgent(string agentId, string callbackUrl);
+    }
+
+    public class CallCreator : ICallCreator
+    {
+        private readonly TwilioRestClient _client;
+        public CallCreator(TwilioRestClient client)
+        {
+            _client = client;
+        }
+
+        public void CallAgent(string agentId, string callbackUrl)
+        {
+            // TODO: Pull out the from number from the configuration file.
+            const string from = "";
+            _client.InitiateOutboundCall(from, string.Format("client:{0}", agentId), callbackUrl);
+        }
+    }
+}
