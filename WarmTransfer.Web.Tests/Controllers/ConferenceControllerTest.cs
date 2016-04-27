1dd180391e3909d1fb822f5bbd82077071b1e8c3
Marcos Sampaio <mosampaio@gmail.com>
2016-04-27 18:17:56 -0300

    Adds ConferenceController.ConnectClient method

---
+++ WarmTransfer.Web.Tests/Controllers/ConferenceControllerTest.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using NUnit.Framework;
+using System.Xml.XPath;
+using TestStack.FluentMVCTesting;
+using WarmTransfer.Web.Controllers;
+using WarmTransfer.Web.Domain;
+using WarmTransfer.Web.Models.Repository;
+using WarmTransfer.Web.Tests.Extensions;
+
+namespace WarmTransfer.Web.Tests.Controllers
+{
+    public class ConferenceControllerTest
+    {
+        private Mock<ICallCreator> _mockCallCreator;
+        private Mock<ICallsRepository> _mockCallsRepository;
+        private ConferenceController _controller;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _mockCallCreator = new Mock<ICallCreator>();
+            _mockCallsRepository = new Mock<ICallsRepository>();
+
+            _controller = new ConferenceController(
+                _mockCallCreator.Object, _mockCallsRepository.Object);
+        }
+
+        [Test]
+        public void WhenConnectClient_ThenShouldCallAgent()
+        {
+            _controller.ConnectClient("conference-id");
+
+            _mockCallCreator.Verify(c => c.CallAgent("agent1", "callback-url"), 
+                Times.Once());
+        }
+
+        [Test]
+        public void WhenConnectClient_ThenItShouldCreateACall()
+        {
+            _controller.ConnectClient("conference-id");
+
+            _mockCallsRepository.Verify(c => c.CreateIfNotExists("agent1", "conference-id"),
+                Times.Once());
+        }
+
+        [Test]
+        public void WhenConnectClient_ThenItShouldGenerateConnectConference()
+        {
+            _controller.WithCallTo(c => c.ConnectClient("conference-id"))
+            .ShouldReturnTwiMLResult(data =>
+                {
+                    Assert.That(data.XPathSelectElement("Response/Dial/Conference").Value, Is.EqualTo("conference-id"));
+                });
+
+        }
+    }
+}
---
+++ WarmTransfer.Web/Controllers/ConferenceController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Twilio.TwiML.Mvc;
+using WarmTransfer.Web.Domain;
+using WarmTransfer.Web.Models.Repository;
+
+namespace WarmTransfer.Web.Controllers
+{
+    public class ConferenceController : Controller
+    {
+        private readonly ICallCreator _callCreator;
+        private readonly ICallsRepository _callsRepository;
+        private readonly ITwiMLGenerator _twiMLGenerator;
+
+        public ConferenceController(
+            ICallCreator callCreator, ICallsRepository callsRepository)
+        {
+            _callCreator = callCreator;
+            _callsRepository = callsRepository;
+            _twiMLGenerator = new TwiMLGenerator();
+        }
+
+        public ActionResult ConnectClient(string conferenceId)
+        {
+            const string agentOne = "agent1";
+            const string callBackUrl = "callback-url"; //TODO extract it to a config file
+            _callCreator.CallAgent(agentOne, callBackUrl);
+            _callsRepository.CreateIfNotExists(agentOne, conferenceId);
+            var response = _twiMLGenerator.GenerateConnectConference(conferenceId, "wait-url", false, true);
+            return new TwiMLResult(response);
+        }
+    }
+}
