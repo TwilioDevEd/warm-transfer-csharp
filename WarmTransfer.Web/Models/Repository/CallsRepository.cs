4f1750b476ff4260b5869c698d91a1d59b463460
Marcos Sampaio <mosampaio@gmail.com>
2016-04-28 11:39:05 -0300

    Add FindByAgentId method to repository

--- WarmTransfer.Web/Models/Repository/CallsRepository.cs
+++ WarmTransfer.Web/Models/Repository/CallsRepository.cs
@@ -1,10 +1,13 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 
 namespace WarmTransfer.Web.Models.Repository
 {
     public interface ICallsRepository
     {
         int CreateIfNotExists(string agentId, string conferenceId);
+
+        Call FindByAgentId(string agentId);
     }
 
     public class CallsRepository : ICallsRepository
@@ -27,5 +30,9 @@ namespace WarmTransfer.Web.Models.Repository
             return _context.SaveChanges();
         }
 
+        public Call FindByAgentId(string agentId)
+        {
+            return _context.Calls.Where(c => c.AgentId == agentId).First();
+        }
     }
 }
