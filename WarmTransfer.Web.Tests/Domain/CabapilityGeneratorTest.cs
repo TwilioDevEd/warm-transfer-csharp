080c0a30428a16bc5b328a1799dc2bdeeed02b26
Marcos Sampaio <mosampaio@gmail.com>
2016-04-28 14:43:26 -0300

    Add CapabilityGenerator class

---
+++ WarmTransfer.Web.Tests/Domain/CabapilityGeneratorTest.cs
@@ -0,0 +1,20 @@
+﻿using Moq;
+using NUnit.Framework;
+using Twilio;
+using WarmTransfer.Web.Domain;
+
+namespace WarmTransfer.Web.Tests.Domain
+{
+    public class CabapilityGeneratorTest
+    {
+        [Test]
+        public void WhenGenerate_ThenItShouldGenerateATwilioCapability()
+        {
+            CapabilityGenerator capabilityGenerator = new CapabilityGenerator();
+
+            var token = capabilityGenerator.Generate("agentId1");
+
+            Assert.IsNotNull(token);
+        }
+    }
+}
---
+++ WarmTransfer.Web/Domain/TwilioCapability.cs
@@ -0,0 +1,20 @@
+﻿using Twilio;
+
+namespace WarmTransfer.Web.Domain
+{
+    public class CapabilityGenerator
+    {
+        private readonly TwilioCapability _twilioCapability;
+
+        public CapabilityGenerator()
+        {
+            _twilioCapability = new TwilioCapability(Config.AccountSID, Config.AuthToken);
+        }
+
+        public string Generate(string agentId)
+        {
+            _twilioCapability.AllowClientIncoming(agentId);
+            return _twilioCapability.GenerateToken();
+        }
+    }
+}
