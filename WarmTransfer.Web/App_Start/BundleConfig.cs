b7894d2943abadcc507fc458a7e9a43eebf61db6
Martin Mena <mmena@twilio.com>
2021-01-25 06:32:20 -0800

    Update the dependencies and refactor the frontend (#96)
    * Update the dependencies and refactor the frontend
    
    * Add LICENSE and other missing .md files
    
    * Add .mergify config

--- WarmTransfer.Web/App_Start/BundleConfig.cs
+++ WarmTransfer.Web/App_Start/BundleConfig.cs
@@ -7,27 +7,14 @@ namespace WarmTransfer.Web
         // For more information on bundling, visit http://go.microsoft.com/fwlink/?LinkId=301862
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Use the development version of Modernizr to develop with and learn from. Then, when you're
-            // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/warm-transfer").Include(
+            bundles.Add(new ScriptBundle("~/bundles/scripts").Include(
+                      "~/Scripts/lib/dist/js/jquery.js",
+                      "~/Scripts/lib/dist/js/bootstrap.js",
                       "~/Scripts/warm-transfer.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/font-awesome.css",
+                      "~/Scripts/lib/dist/css/bootstrap.css",
+                      "~/Scripts/lib/dist/css/font-awesome.css",
                       "~/Content/site.css"));
         }
     }
